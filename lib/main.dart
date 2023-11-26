d3bb9d08cdd3a154700d05cd0a7c0b22a65b7f6d
Jhojan <jhojanrock98@gmail.com>
2023-11-26 13:40:10 -0500

    Creación del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: const MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+   void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {           // ← 1
+    var appState = context.watch<MyAppState>();  // ← 2
+
+    return Scaffold(                             // ← 3
+      body: Column(                              // ← 4
+        children: [
+          const Text('A random AWESOME idea:'),        // ← 5
+          Text(appState.current.asLowerCase),    // ← 6
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: const Text('Next'),
+          ),
+           ElevatedButton(
+            onPressed: () {
+            appState.getNext();  // ← This instead of print().
+          },
+          child: const Text('Next'),
+          ),
+        ],                                       // ← 7
+      ),
+    );
+  }
+}
